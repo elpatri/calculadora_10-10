4ba8b625da60457b50c499340f433ed634635aea
Alysson <alyssonkauan5@hotmail.com>
2018-10-10 09:59:47 -0400

    Persistence arrumado

--- calculadora_10-10/src/br/edu/ifro/model/Usuario.java
+++ calculadora_10-10/src/br/edu/ifro/model/Usuario.java
@@ -9,4 +9,34 @@ import javax.persistence.Id;
 
 public class Usuario {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id_usu;
+    private String username_usu;
+    private String senha_usu;
+
+    // GETTERS E SETTERS
+        public Integer getId_usu() {
+            return id_usu;
+        }
+
+        public void setId_usu(Integer id_usu) {
+            this.id_usu = id_usu;
+        }
+
+        public String getSenha_usu() {
+            return senha_usu;
+        }
+
+        public void setSenha_usu(String senha_usu) {
+            this.senha_usu = senha_usu;
+        }
+
+        public String getUsername_usu() {
+            return username_usu;
+        }
+
+        public void setUsername_usu(String username_usu) {
+            this.username_usu = username_usu;
+        }
 }
