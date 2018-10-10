fb26d95e7b2e17a5fd6a13a58a3559ce010e102a
Alysson <alyssonkauan5@hotmail.com>
2018-10-10 11:08:45 -0400

    Tela e Controller de Calculadora

--- calculadora_10-10/src/br/edu/ifro/control/CalculadoraController.java
+++ calculadora_10-10/src/br/edu/ifro/control/CalculadoraController.java
@@ -7,7 +7,10 @@ package br.edu.ifro.control;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -16,12 +19,56 @@ import javafx.fxml.Initializable;
  */
 public class CalculadoraController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private TextField txtResultado;
+    @FXML
+    private Button btnLimparTudo;
+    @FXML
+    private Button btn7;
+    @FXML
+    private Button btn8;
+    @FXML
+    private Button btn9;
+    @FXML
+    private Button btn4;
+    @FXML
+    private Button btn5;
+    @FXML
+    private Button btn6;
+    @FXML
+    private Button btn1;
+    @FXML
+    private Button btn2;
+    @FXML
+    private Button btn3;
+    @FXML
+    private Button btnSomar;
+    @FXML
+    private Button btnMultiplicar;
+    @FXML
+    private Button btnSubtrair;
+    @FXML
+    private Button btnDividir;
+    @FXML
+    private Button btnResultado;
+    @FXML
+    private Button btn0;
+    @FXML
+    private Button btnPonto;
+    @FXML
+    private Button btnUsuario;
+    @FXML
+    private Button Historico;
+    
+    private Integer num1;
+    private Integer num2;
+    private String sinal;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    
     
 }
